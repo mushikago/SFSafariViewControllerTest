5cbfcca8cf5b3d79c887dae2a4cb21c6a27572f2
tshiraishi <tshiraishi@mushikago.com>
2015-09-08 17:49:23 +0900

    1st

---
+++ SFSafariViewControllerTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SFSafariViewControllerTest
+//
+//  Created by Tetsuya Shiraishi on 2015/09/08.
+//  Copyright © 2015年 Tetsuya Shiraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func openBtn(sender: AnyObject) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
