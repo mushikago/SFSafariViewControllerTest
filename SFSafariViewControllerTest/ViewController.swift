4bef3e4e0137fbf99b11063ccd4403eb120b2fa0
tshiraishi <tshiraishi@mushikago.com>
2015-09-08 18:48:23 +0900

    2nd

--- SFSafariViewControllerTest/ViewController.swift
+++ SFSafariViewControllerTest/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     @IBAction func openBtn(sender: AnyObject) {
         
+        if #available(iOS 9.0, *) {
+            let _url:NSURL = NSURL(string: "http://adobe.com")!
+            let _brow = SFSafariViewController(URL: _url, entersReaderIfAvailable: true)
+            _brow.delegate = self
+            presentViewController(_brow, animated: true, completion: nil)
+        } else {
+            // Fallback on earlier versions
+            let _alert = UIAlertView()
+            _alert.title = "SFSafariViewControllerはiOS9以上で動作します。"
+            _alert.addButtonWithTitle("OK")
+            _alert.show()
+        }
+        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +38,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @available(iOS 9.0, *)
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        print("close")
+    }
 }
 
